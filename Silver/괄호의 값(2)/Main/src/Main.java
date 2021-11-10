8112563c476d7cd650050992e682546c30e0aa5f
bana_na21 <bana_na21@naver.com>
2021-11-10 23:47:48 +0900

    자바 알고리즘 문제풀이

--- Silver/괄호의 값(2)/Main/src/Main.java
+++ Silver/괄호의 값(2)/Main/src/Main.java
@@ -9,19 +9,33 @@ public class Main {
     }
 
     public static int solution(String s) {
-        int answer = 0;
-        int temp = 0;
-        Stack<Character> st = new Stack<Character>();
 
-        for (Character c : s.toCharArray()) {
-            if (c == '(' || c == '[') {
+        int mul = 1; // 'multiplication' 곱셈
+        int sum = 0;
+
+        Stack<Character> st = new Stack<>();
+
+        for(Character c : s.toCharArray()){
+            if(c == '(' || c == '['){
                 st.push(c);
-            } else if (c == ')') {
-            
-            } else if (c == ']') {
+                if(c == '('){
+                    mul *= 2;
+                }else{
+                    mul *= 3;
+                }
+            }else{
+                Character c1 = st.pop();
                 
+                if(st.isEmpty() || c == ')' && c1 == '[') return 0;
+                if(st.isEmpty() || c == ']' && c1 == '(') return 0;
+
+                if(c1 == '(' && c == ')'){
+                    st.push((char)2);
+                }else if(c1 == '[' && c == ']'){
+                    st.push((char)3);
+                }
             }
         }
-        return answer;
+        return sum;
     }
 }
