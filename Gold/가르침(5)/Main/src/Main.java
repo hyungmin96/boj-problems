59b5e2ad6e590ee1e4acf69fda8a35a4101c444d
bana_na21 <bana_na21@naver.com>
2021-11-13 00:19:34 +0900

    자바 알고리즘 문제풀이

--- Gold/가르침(5)/Main/src/Main.java
+++ Gold/가르침(5)/Main/src/Main.java
@@ -15,6 +15,7 @@ public class Main {
         // a, n, t, c, i
         // 97, 110, 116, 99, 105
         String[] array = new String[n];
+        boolean[] visited = new boolean[26];
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
             if (input.substring(0, 4).equals("anta") && input.substring(input.length() - 4).equals("tica")) {
@@ -26,42 +27,44 @@ public class Main {
             array[i] = input;
         }
 
-        for (int i = 97; i < 121; i++) {
-            dfs(0, i, k, "", array);
-        }
+        visited[0] = true;
+        visited[2] = true;
+        visited[8] = true;
+        visited[13] = true;
+        visited[19] = true;
+
+        dfs(0, 0, k - 5, "antic", array, visited);
         System.out.println((answer <= 0) ? 0 : answer);
+
     }
 
-    public static void dfs(int depth, int index, int level, String curr, String[] array) {
+    public static void dfs(int depth, int index, int level, String curr, String[] array, boolean[] visited) {
 
-        if (level < 0)
-            return;
+        if(level < 0) return;
 
-        if (depth == level) {
+        if(depth == level){
             int sum = 0;
-
-            for (String item : array) {
+            for(String item : array){
                 boolean isLearned = true;
-                if (item.equals("NONE")) {
-                    isLearned = false;
-                } else {
-                    for (int i = 0; i < item.length(); i++) { // 내가 배운 단어들
-                        if (!curr.contains(String.valueOf(item.charAt(i)))) { // 배우지 않은 단어가 포함된 단어일 경우
-                            isLearned = false;
-                            System.out.println(String.valueOf(item.charAt(i)));
-                            break;
-                        }
+                for(char c : item.toCharArray()){
+                    if(!curr.contains(String.valueOf(c))){
+                        isLearned = false;
+                        break;
                     }
                 }
-
-                if (isLearned) sum ++;
+                if(isLearned) sum++;
             }
-
             answer = (sum > answer) ? sum : answer;
             return;
-        } else {
-            curr += (char) (index);
-            dfs(depth + 1, index + 1, level, curr, array);
+        }else{
+            for(int i = index; i < 26; i ++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    dfs(depth + 1, i + 1, level, curr + (char)(i + 97), array, visited);
+                    visited[i] = false;
+                }
+            }
         }
+
     }
 }
