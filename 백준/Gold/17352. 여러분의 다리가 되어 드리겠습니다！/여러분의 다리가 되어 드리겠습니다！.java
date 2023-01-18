af42627c63f9b646818d79dbe4ee0f695974cc9b
작은꼬마개발자 <bana_na21@naver.com>
2023-01-18 21:45:40 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 560 ms, Memory: 88396 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -3,34 +3,53 @@ import java.util.*;
 
 public class Main {
 
+    static int[] nodes;
     static ArrayList<Integer>[] vertex;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
+        nodes = new int[n + 1];
         vertex = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i ++) vertex[i] = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i ++) {
+            nodes[i] = i;   
+            vertex[i] = new ArrayList<>();
+        }
+
         for (int i = 0; i < n - 2; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            vertex[start].add(end);
-            vertex[end].add(start);
+            // vertex[start].add(end);
+            // vertex[end].add(start);
+            union(start, end);
         }
 
         int root = 1;
-        boolean[] check = new boolean[n + 1];
-        check[root] = true;
-        dfs(1, check);
+        // boolean[] check = new boolean[n + 1];
+        // check[root] = true;
+        // dfs(1, check);
         for (int i = 1; i <= n; i ++){
-            if(!check[i]){
-                System.out.println(root + " " + i + "\n");
+            if(find(nodes[i]) != root){
+                System.out.println(root + " " + find(nodes[i]));
                 return;
             }
         }
+    }
+    
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a < b) nodes[b] = a;
+        else nodes[a] = b;
+    }
+
+    public static int find(int a){
+        if(nodes[a] == a) return a;
+        return nodes[a] = find(nodes[a]);
     }
 
     public static void dfs(int node, boolean[] check){
