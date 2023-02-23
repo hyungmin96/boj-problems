6d762c9537611d89c4c1d676eca076afc97e1e3c
작은꼬마개발자 <bana_na21@naver.com>
2023-02-23 09:44:21 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 31.96 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -4,42 +4,39 @@ class Solution {
         long answer = 0;
         Stack<Integer> d = new Stack<>();
         Stack<Integer> p = new Stack<>();
-        int box = 0;
         
-        for(int i = 0; i < n; i ++){
+        for(int i = 0; i < deliveries.length; i ++){
             d.push(deliveries[i]);
             p.push(pickups[i]);
         }
         
-        while (!d.empty() && d.peek() == 0) 
-            d.pop();
-        while (!p.empty() && p.peek() == 0) 
-            p.pop();
         
         while(!d.isEmpty() || !p.isEmpty()){
-            answer += Math.max(d.size() * 2, p.size() * 2);
+            int cargo = 0;
+            int box = 0;
             
-            box = 0;
-            while(!d.isEmpty() && box <= cap){
-                if(d.peek() + box <= cap){
-                    box += d.peek();
-                }else{
-                    d.push(d.pop() - (cap - box));
+            while(!d.isEmpty()) if(d.peek() == 0) d.pop(); else break;
+            while(!p.isEmpty()) if(p.peek() == 0) p.pop(); else break;
+            
+            int dist = Math.max(d.size(), p.size()) * 2;
+            while(!d.isEmpty()){
+                if(d.peek() + cargo <= cap)
+                    cargo += d.pop();  
+                else{
+                    d.push(d.pop() - (cap - cargo));
                     break;
                 }
-                d.pop();
             }
-            
-            box = 0;
-            while(!p.isEmpty() && box <= cap){
-                if(p.peek() + box <= cap){
-                    box += p.peek();
-                }else{
+            while(!p.isEmpty()){
+                if(p.peek() + box <= cap)
+                    box += p.pop();
+                else{
                     p.push(p.pop() - (cap - box));
                     break;
                 }
-                p.pop();
             }
+            
+            answer += dist;
         }
         
         return answer;
