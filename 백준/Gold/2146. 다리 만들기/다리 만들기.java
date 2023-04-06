9f8c74840a0a84e69065fbf96942b44230d4a047
작은꼬마개발자 <bana_na21@naver.com>
2023-04-06 22:26:44 +0900

    [Gold III] Title: 다리 만들기, Time: 2620 ms, Memory: 295812 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -5,14 +5,13 @@ public class Main {
 
     static int N, min = 987654321;
     static boolean[][] check;
-    static int[][] map, idx;
+    static int[][] map;
     static int[][] dirs = {{ -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }};
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        idx = new int[N][N];
         check = new boolean[N][N];
 
         for(int i = 0; i < N; i ++){
@@ -22,10 +21,10 @@ public class Main {
             }
         }
 
-        int cnt = 0;
+        int cnt = 2;
         for(int r = 0; r < N; r ++){
             for(int c = 0; c < N; c ++){
-                if(!check[r][c] && map[r][c] != 0){
+                if(!check[r][c] && map[r][c] == 1){
                     dfs(r, c, cnt);
                     cnt ++;
                 }
@@ -34,20 +33,21 @@ public class Main {
 
         for(int r = 0; r < N; r ++){
             for(int c = 0; c < N; c ++){
-                if(map[r][c] == 1){
-                    dijkstra(idx[r][c], r, c);
+                if(map[r][c] >= 2){
+                    dijkstra(r, c);
                 }
             }
         }
         System.out.println(min);
     }
 
-    public static void dijkstra(int land, int r, int c){
+    public static void dijkstra(int r, int c){
         boolean[][] v2 = new boolean[N][N];
         Queue<int[]> que = new LinkedList<>();
         que.offer(new int[] { r, c, 0 });
-        v2[r][c] = true;
 
+        int land = map[r][c];
+        v2[r][c] = true;
         while(!que.isEmpty()){
             int[] cur = que.poll();
             for(int[] d : dirs){
@@ -55,7 +55,7 @@ public class Main {
                 int nc = cur[1] + d[1];
                 if(isOutRange(nr, nc) || v2[nr][nc]) continue;
                 v2[nr][nc] = true;
-                if(map[nr][nc] == 1 && idx[nr][nc] != land){
+                if(map[nr][nc] != 0 && map[nr][nc] != land){
                     min = Math.min(cur[2], min);
                     return;
                 }
@@ -65,13 +65,21 @@ public class Main {
     }
 
     public static void dfs(int r, int c, int cnt){
-        idx[r][c] = cnt;
-        for(int[] d : dirs){
-            int nr = r + d[0];
-            int nc = c + d[1];
-            if(isOutRange(nr, nc) || map[nr][nc] == 0 || check[nr][nc]) continue;
-            check[nr][nc] = true;
-            dfs(nr, nc, cnt);
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[] { r, c });
+        map[r][c] = cnt;
+        check[r][c] = true;
+
+        while(!que.isEmpty()){
+            int[] cur = que.poll();
+            for(int[] d : dirs){
+                int nr = cur[0] + d[0];
+                int nc = cur[1] + d[1];
+                if(isOutRange(nr, nc) || map[nr][nc] == 0 || check[nr][nc]) continue;
+                map[nr][nc] = cnt;
+                check[nr][nc] = true;
+                que.offer(new int[] { nr, nc });
+            }
         }
     }
 
