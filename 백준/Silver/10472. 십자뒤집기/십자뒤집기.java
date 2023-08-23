f76da2db17ce7edc8e3427646556bf2b9ec04210
BHM <bana_na21@naver.com>
2023-08-23 09:54:10 +0900

    [Silver I] Title: 십자뒤집기, Time: 164 ms, Memory: 18208 KB -BaekjoonHub

--- 백준/Silver/10472. 십자뒤집기/십자뒤집기.java
+++ 백준/Silver/10472. 십자뒤집기/십자뒤집기.java
@@ -11,66 +11,64 @@ public class Main {
 
 class Solution {
 
-    public class Pair{
-        int cnt;
-        String str;
-        public Pair(int cnt, String str){
-            this.cnt = cnt;
-            this.str = str;
-        }
-    }
     int P;
-    HashSet<String> v = new HashSet<>();
-    int[][] dirs = { { 0, 0 }, { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, - 1}};
+    char[][] map = new char[3][3];
+    int[][] dirs = {{ -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { 0, 0 }};
 
     public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         P = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < P; i ++){
-            v = new HashSet<>();
-            StringBuilder sb2 = new StringBuilder();
-            for(int j = 0; j < 3; j ++)
-                sb2.append(br.readLine());
-
-            sb.append(bfs(sb2.toString()) + "\n");
+        while(P -- > 0){
+            for(int i = 0; i < 3; i ++){
+                String str = br.readLine();
+                for(int j = 0; j < 3; j ++){
+                    map[i][j] = str.charAt(j);
+                }
+            }
+            
+            int min = dfs(0, 0);
+            sb.append(min + "\n");
         }
+
         System.out.println(sb.toString());
     }
 
-    public int bfs(String str){
-       Queue<Pair> q = new LinkedList<>();
-       q.offer(new Pair(0, str));
-       v.add(str);
-
-       while (!q.isEmpty()) {
-           Pair p = q.poll();
-           if(p.str.equals(".........")){
-                return p.cnt;
-           }
-
-           for(int i = 0; i < 9; i ++){
-               int r = i / 3;
-               int c = i % 3;
-               char[] tmp = p.str.toCharArray();
-               for (int d = 0; d < 5; d ++) {
-                   int nr = r + dirs[d][0];
-                   int nc = c + dirs[d][1];
-                   if (nr < 0 || nc < 0 || nr >= 3 || nc >= 3) continue;
-                   tmp[nr * 3 + nc] = tmp[nr * 3 + nc] == '.' ? '*' : '.';
+    public int dfs(int r, int c){
+        int result = 987654321;
+        if(r >= 3){
+            for(int i = 0; i < 3; i ++){
+                for(int j = 0; j < 3; j ++){
+                    if(map[i][j] == '*'){
+                        return 987654321;
+                    }
                 }
-                if(v.contains(convert(tmp))) continue;
-                v.add(convert(tmp));
-                q.offer(new Pair(p.cnt + 1, convert(tmp)));
-           }
-       }
-       return 987654321;
+            }
+            return 0;
+        }
+
+        int nr = r, nc = c + 1;
+        if(nc >= 3){
+            nr = r + 1;
+            nc = 0;
+        }
+
+        result = Math.min(result, dfs(nr, nc));
+        click(r, c);
+
+        result = Math.min(result, dfs(nr, nc) + 1);
+        click(r, c);
+
+        return result;
     }
 
-    public String convert(char[] tmp){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < tmp.length; i ++)
-            sb.append(tmp[i]);
-        return sb.toString();
+    public void click(int r, int c){
+        for(int d = 0; d < 5; d ++){
+            int nr = r + dirs[d][0];
+            int nc = c + dirs[d][1];
+            if(nr < 0 || nc < 0 || nr >= 3 || nc >= 3) continue;
+            map[nr][nc] = (map[nr][nc] == '.') ? '*' : '.';
+        }
     }
 }
+
