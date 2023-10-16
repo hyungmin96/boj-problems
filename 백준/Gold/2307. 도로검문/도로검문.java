16421a52da60dce6f37a61d4477faff8debe5201
BHM <bana_na21@naver.com>
2023-10-16 14:49:13 +0900

    [Gold I] Title: 도로검문, Time: 576 ms, Memory: 54208 KB -BaekjoonHub

--- 백준/Gold/2307. 도로검문/도로검문.java
+++ 백준/Gold/2307. 도로검문/도로검문.java
@@ -1,9 +1,7 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         Solution sol = new Solution();
         sol.solution();
     }
@@ -11,9 +9,9 @@ public class Main {
 
 class Solution {
 
-    public int N, M, dist = 987654321;
-    public int[] v;
-    public ArrayList<int[]>[] vertex;
+    int N, M, min = 987654321, answer = -2;
+    ArrayList<int[]>[] vertex;
+    ArrayList<Integer> cp = new ArrayList<>();
 
     public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,106 +19,74 @@ class Solution {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        v = new int[N + 1];
         vertex = new ArrayList[N + 1];
-
         for(int i = 1; i <= N; i ++) vertex[i] = new ArrayList<>();
+
         for(int i = 0; i < M; i ++){
             st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
 
-            int to = Integer.parseInt(st.nextToken());    
-            int from = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            vertex[to].add(new int[] { from, cost });
-            vertex[from].add(new int[] { to, cost });
+            vertex[s].add(new int[] { e, t });
+            vertex[e].add(new int[] { s, t });
         }
 
-        int max = -1;
-        int[] path = getPath();
-        for(int i = 0; i < path.length - 1; i ++){
-            int n1 = path[i];
-            int n2 = path[i + 1];
+        getDistance(true, 0, 0);
 
-            int cur_dist = bfs(n1, n2);
-            if(cur_dist == -1){
-                System.out.println(-1);
-                return;
-            }else{
-                max = Math.max(cur_dist - dist, max);
-            }
+        for(int i = 0; i < cp.size() - 1; i ++){
+            int n1 = cp.get(i);
+            int n2 = cp.get(i + 1);
+            getDistance(false, n1, n2);
         }
-
-        System.out.println(max);
+        System.out.println(answer == -2 ? -1 : answer);
     }
 
-    public int bfs(int n1, int n2){
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+    public int getDistance(boolean getPath, int n1, int n2){
+        int[] path = new int[N + 1];
+        int[] v = new int[N + 1];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
             @Override
             public int compare(int[] o1, int[] o2){
-                return o1[1] - o2[1];
+                return o1[0] - o2[0];
             }
         });
 
-        Arrays.fill(v, Integer.MAX_VALUE);
-        pq.offer(new int[] { 1, 0 });
+        // [소요시간, 방문노드]
+        pq.offer(new int[] { 0, 1 });
         v[1] = 0;
+        Arrays.fill(v, 987654321);
 
         while(!pq.isEmpty()){
             int[] cur = pq.poll();
-            if(cur[0] == N){
-                return cur[1];
-            }
-            for(int[] next : vertex[cur[0]]){
-                if((next[0] == n1 && cur[0] == n2) || (next[0] == n2 && cur[0] == n1)) continue;
-                if(v[next[0]] > cur[1] + next[1]){
-                    v[next[0]] = cur[1] + next[1];
-                    pq.offer(new int[] { next[0], v[next[0]] });
+            if(cur[1] == N){
+                if(getPath){
+                    dfs(N, path);
+                    return min = cur[0];
+                }else{
+                    return answer = Math.max(answer, cur[0] - min);
                 }
+            }
+            for(int[] next : vertex[cur[1]]){
+                if(!getPath){
+                    if((cur[1] == n1 && next[0] == n2) || cur[1] == n2 || next[0] == n1) 
+                        continue;
+                }
+                if(v[next[0]] <= cur[0] + next[1]) continue;
+                v[next[0]] = cur[0] + next[1];
+                if(getPath) path[next[0]] = cur[1];
+                pq.offer(new int[] { cur[0] + next[1], next[0] });
             }
         }
         return -1;
     }
 
-    public int[] getPath(){
-        // from, cost, depth
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] o1, int[] o2){
-                return o1[1] - o2[1];
-            }
-        });
-
-        int[] path = new int[5001];
-        Arrays.fill(v, Integer.MAX_VALUE);
-        pq.offer(new int[] { 1, 0, 0 });
-        v[1] = 0;
-
-        while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            if(cur[0] == N){
-                dist = cur[1];
-                break;
-            }
-            for(int[] next : vertex[cur[0]]){
-                if(v[next[0]] > cur[1] + next[1]){
-                    v[next[0]] = cur[1] + next[1];
-                    path[next[0]] = cur[0];
-                    pq.offer(new int[] { next[0], v[next[0]], cur[2] + 1 });
-                }
-            }
-        }
-
-        int idx = N;
-        ArrayList<Integer> edges = new ArrayList<>();
-        while(true){
-            edges.add(idx);
-            if(path[idx] == 0) break;
-            idx = path[idx];
-        }
-
-        int[] temp = new int[edges.size()];
-        for(int i = 0; i < edges.size(); i ++) temp[i] = edges.get(i);
-        return temp;
+    public void dfs(int node, int[] path){
+        if(node == 0) return;
+        cp.add(node);
+        if(node == 1) return;
+        dfs(path[node], path);
     }
 }
+
