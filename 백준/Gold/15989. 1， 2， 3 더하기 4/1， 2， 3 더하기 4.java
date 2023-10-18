077849594eaef331b6dcd1a6130a38324b8cc275
BHM <bana_na21@naver.com>
2023-10-18 16:21:04 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 196 ms, Memory: 30788 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -21,17 +21,28 @@ class Solution {
         for(int i = 0; i < N; i ++){
             dp = new int[10001];
             int num = Integer.parseInt(br.readLine());
-
+            Arrays.fill(dp, -1);
             dp[1] = 1;
             dp[2] = 2;
             dp[3] = 3;
-            for(int j = 4; j <= num; j ++){
-                dp[j] = (j / 2) + dp[j - 3] + 1;
-            }
-
+            // for(int j = 4; j <= num; j ++){
+            //     dp[j] = (j / 2) + dp[j - 3] + 1;
+            // }
+            dfs(num, dp);
             sb.append(dp[num] + "\n");
         }
         System.out.println(sb.toString());
+    }
+
+    public int dfs(int cur, int[] dp){
+        if(cur <= 0){
+            return 0;
+        }
+
+        if(dp[cur] != -1) return dp[cur];
+        dp[cur] = Math.max(dp[cur], cur / 2 + dfs(cur - 3, dp) + 1);
+
+        return dp[cur];
     }
 }
 
