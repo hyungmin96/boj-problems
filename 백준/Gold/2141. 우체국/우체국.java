b91a0ff0fe20b09870251d789dcfebde7800db80
BHM <bana_na21@naver.com>
2023-10-25 21:53:20 +0900

    [Gold IV] Title: 우체국, Time: 536 ms, Memory: 46136 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -1,64 +1,54 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         Solution sol = new Solution();
         sol.solution();
     }
 }
 
 class Solution {
-
+    
     int N;
+    int[][] house;
 
     public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        house = new int[N][2];
 
-        int[][] arr = new int[N][2];
+        long tmp = 0;
         for(int i = 0; i < N; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int pos = Integer.parseInt(st.nextToken());
-            int population = Integer.parseInt(st.nextToken());
 
-            arr[i] = new int[] { pos, population };
+            int pos = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            house[i][0] = pos;
+            house[i][1] = p;
+            tmp += p;
         }
 
-        Arrays.sort(arr, new Comparator<int[]>(){
+        Arrays.sort(house, new Comparator<>(){
             @Override
             public int compare(int[] o1, int[] o2){
                 return o1[0] - o2[0];
             }
         });
-
-        long tmp = 0;
-        long[] sum = new long[N];
-        for(int i = 0; i < arr.length; i ++){
-            tmp += arr[i][1];
-            sum[i] = tmp;
-        }
-
-        System.out.println(binarySearch(sum, arr));
+        System.out.println(solve(tmp));
     }
 
-    public long binarySearch(long[] sum, int[][] arr){
-        long ret = 987654321;
-        int l = 0, r = arr.length - 1;
-        while(l <= r){
-            int mid = (r + l) / 2;
-            long p_l = sum[mid];
-            long p_r = sum[arr.length - 1] - sum[mid];
-
-            if(p_l >= p_r){
-                ret = Math.min(ret, arr[mid][0]);
-                r = mid - 1;
-            }else{
-                l = mid + 1;
+    public int solve(long tmp){
+        int pos = Integer.MAX_VALUE;
+        long cnt = 0;
+        for(int i = 0; i < N; i ++){
+            cnt += house[i][1];
+            if((tmp + 1) / 2 <= cnt){
+                return house[i][0];
             }
         }
 
-        return ret;
+        return pos;
     }
 }
+
