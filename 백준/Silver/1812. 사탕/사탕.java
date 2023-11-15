81cf792246cf7b28792d7202d149266dfd8a35e5
BHM <bana_na21@naver.com>
2023-11-15 22:47:20 +0900

    [Silver IV] Title: 사탕, Time: 188 ms, Memory: 16232 KB -BaekjoonHub

--- 백준/Silver/1812. 사탕/사탕.java
+++ 백준/Silver/1812. 사탕/사탕.java
@@ -6,54 +6,55 @@ public class Main{
         sol.solution();
     }
 }
-    
-class Solution{
 
+class Solution {
+    
     int N;
-    int[] sum, answer;
+    int[] answer, sum;
 
     public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
         sum = new int[N];
         answer = new int[N];
 
         for(int i = 0; i < N; i ++){
-            int num = Integer.parseInt(br.readLine());
-            sum[i] = num;
+            sum[i] = Integer.parseInt(br.readLine());
         }
-
+        
         dfs(0, new int[N]);
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < answer.length; i ++){
+        for(int i = 0; i < answer.length; i ++)
             sb.append(answer[i] + "\n");
-        }
 
         System.out.println(sb.toString());
     }
 
-    public void dfs(int depth, int[] tmp){
-        if(depth >= N){
-            if(tmp[0] + tmp[N - 1] == sum[N - 1]){
-                answer = tmp.clone();
+    public boolean dfs(int depth, int[] arr){
+        if(depth == N){
+            if(arr[0] + arr[N - 1] == sum[N - 1]){
+                answer = arr;
+                return true;
             }
-            return;
+
+            return false;
         }
 
         if(depth == 0){
-            for(int i = 0; i <= sum[0]; i ++){
-                tmp[0] = i;
-                if(sum[N - 1] - i < 0) break;
-                tmp[N - 1] = sum[N - 1] - tmp[0];
-                dfs(depth + 1, tmp);
+            for(int i = 0; i <= sum[depth]; i ++){
+                if(sum[N - 1] - i >= 0){
+                    arr[depth] = i;
+                    if(dfs(depth + 1, arr)) return true;
+                }
             }
         }else{
-            int pre_idx = depth - 1;
-            if(sum[pre_idx] - tmp[pre_idx] < 0) return;
-            tmp[depth] = sum[pre_idx] - tmp[pre_idx];
-            dfs(depth + 1, tmp);
+            if(sum[depth - 1] - arr[depth - 1] >= 0){
+                arr[depth] = sum[depth - 1] - arr[depth - 1];
+                if(dfs(depth + 1, arr)) return true;
+            }
         }
+
+        return false;
     }
 }
+
