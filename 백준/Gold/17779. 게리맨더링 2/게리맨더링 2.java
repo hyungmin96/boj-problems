9312b054f12eb6ef9f74c16ee8da41133cbcf09a
BHM <bana_na21@naver.com>
2023-12-21 22:10:17 +0900

    [Gold III] Title: 게리맨더링 2, Time: 192 ms, Memory: 15400 KB -BaekjoonHub

--- 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
+++ 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         Solution sol = new Solution();
         sol.solution();
@@ -10,36 +9,42 @@ public class Main {
 }
 
 class Solution {
-    
-    public int N;
-    public int[][] map, dirs = {
-        { -1, 0 },
-        { 0, 1 },
-        { 1, 0 },
-        { 0, -1 }
-    };
+
+    int N;
+    int[][] map;
 
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        
-        long cnt = 0;
-        for(int i = 0; i < N; i ++){
+        map = new int[N + 1][N + 1];
+        int total = 0;
+        for(int i = 1; i <= N; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < N; j ++){
-                int num = Integer.parseInt(st.nextToken());
-                map[i][j] = num;
-                cnt += num;
+            for(int j = 1; j <= N; j ++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                total += map[i][j];
             }
         }
 
-        long answer = 987654321;
-        for(int i = 0; i < N - 2; i ++){
-            for(int j = 1; j < N - 1; j ++){
-                for(int d1 = 1; d1 < N - 1; d1 ++){
-                    for(int d2 = 1; d2 < N - 1; d2 ++){
-                        answer = Math.min(answer, dfs(cnt, i, j, d1, d2));
+        int answer = 987654321;
+        for(int x = 1; x <= N; x ++){
+            for(int y = 1; y <= N; y ++){
+                for(int d1 = 1; d1 <= N; d1 ++){
+                    for(int d2 = 1; d2 <= N; d2 ++){
+                        if(y - d1 <= 0) continue;
+                        if(y + d2 > N) continue;
+                        if(x + d1 + d2 > N) continue;
+
+                        int s1 = getSection1(x, y, d1, d2);
+                        int s2 = getSection2(x, y, d1, d2);
+                        int s3 = getSection3(x, y, d1, d2);
+                        int s4 = getSection4(x, y, d1, d2);
+                        int s5 = total - s1 - s2 - s3 - s4;
+
+                        int max = Math.max(s1, Math.max(s2, Math.max(s3, Math.max(s4, s5))));
+                        int min = Math.min(s1, Math.min(s2, Math.min(s3, Math.min(s4, s5))));
+
+                        answer = Math.min(answer, max - min);
                     }
                 }
             }
@@ -47,41 +52,47 @@ class Solution {
         System.out.println(answer);
     }
 
-    public long dfs(long cnt, int r, int c, int d1, int d2){
-        if(c - d1 < 0 || r + d1 + d2 >= N || c + d2 >= N) return 987654321;
-
-        long a1 = 0;
-        for(int i = r + d1 - 1, j = c - d1; i >= 0; i --, j ++){
-            for(int k = 0; k <= Math.min(j, c); k ++){
-                a1 += map[i][k];
+    public int getSection1(int x, int y, int d1, int d2){
+        int cnt = 0;
+        for(int r = x + d1 - 1, c = y - d1; r > 0; r --, c ++){
+            if(c >= y) c = y;
+            for(int i = 1; i <= c; i ++){
+                cnt += map[r][i];
             }
         }
+        return cnt;
+    }
 
-        long a2 = 0;
-        for(int i = r + d2, j = c + d2 + 1; i >= 0; i --, j --){
-            for(int k = Math.max(j, c + 1); k < N; k ++){
-                a2 += map[i][k];
-            }
-        }  
-
-        long a3 = 0;
-        for(int i = r + d1, j = c - d1 - 1; i < N; i ++, j ++){
-            for(int k = 0; k <= Math.min(c - d1 + d2 - 1, j); k ++){
-                a3 += map[i][k];
+    public int getSection2(int x, int y, int d1, int d2){
+        int cnt = 0;
+        for(int r = x + d2, c = y + d2 + 1; r > 0; r --, c --){
+            if(c <= y + 1) c = y + 1;
+            for(int i = c; i <= N; i ++){
+                cnt += map[r][i];
             }
         }
+        return cnt;
+    }
 
-        long a4 = 0;
-        for(int i = r + d2 + 1, j = c + d2; i < N; i ++, j --){
-            for(int k = N - 1; k >= Math.max(c - d1 + d2, j); k --){
-                a4 += map[i][k];
+    public int getSection3(int x, int y, int d1, int d2){
+        int cnt = 0;
+        for(int r = x + d1, c = y - d1 - 1; r <= N; r ++, c ++){
+            if(c == y - d1 + d2) c = y - d1 + d2 - 1;
+            for(int i = 1; i <= c; i ++){
+                cnt += map[r][i];
             }
         }
-        
-        long a5 = cnt - a1 - a2 - a3 - a4;
-        long max = Math.max(a5, Math.max(a1, Math.max(a2, Math.max(a3, a4))));
-        long min = Math.min(a5, Math.min(a1, Math.min(a2, Math.min(a3, a4))));
+        return cnt;
+    }
 
-        return max - min;
+    public int getSection4(int x, int y, int d1, int d2){
+        int cnt = 0;
+        for(int r = x + d2 + 1, c = y + d2; r <= N; r ++, c --){
+            if(c <= y - d1 + d2) c = y - d1 + d2;
+            for(int i = c; i <= N; i ++){
+                cnt += map[r][i];
+            }
+        }
+        return cnt;
     }
 }
