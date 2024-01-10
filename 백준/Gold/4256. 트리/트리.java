484a32047aad8497a313de898a3ee5abc31e10d3
BHM <bana_na21@naver.com>
2024-01-10 14:49:56 +0900

    [Gold II] Title: 트리, Time: 436 ms, Memory: 37200 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -1,57 +1,65 @@
 import java.io.*;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException{
+
+class Main {
+    public static void main(String[] args) throws IOException {
         Solution sol = new Solution();
         sol.solution();
     }
 }
 
-class Solution{
+class Solution {
 
-    int pos = 1;
-    StringBuilder answer = new StringBuilder();
+    int N, depth = 0;
+    int[] pre, in, index;
     StringBuilder sb = new StringBuilder();
-    public void solution() throws IOException{
+
+    public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        for(int i = 0; i < tc; i ++){
-            pos = 1;
-            sb = new StringBuilder();
-            int nodes = Integer.parseInt(br.readLine());
-            int[] pre = new int[nodes];
-            int[] in = new int[nodes];
-            int[] idx = new int[nodes + 1];
+        N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i ++){
+            depth = 0;
+            int num = Integer.parseInt(br.readLine());
+            
+            pre = new int[num];
+            in = new int[num];
+            index = new int[num + 1];
+
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < nodes; j ++){
+            for(int j = 0; j < num; j ++){
                 pre[j] = Integer.parseInt(st.nextToken());
             }
+
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < nodes; j ++){
-                int num = Integer.parseInt(st.nextToken());
-                in[j] = num;
-                idx[num] = j;
+            for(int j = 0; j < num; j ++){
+                in[j] = Integer.parseInt(st.nextToken());
+                index[in[j]] = j;
             }
 
-            dfs(pre[0], -1, nodes, pre, in, idx);
-            answer.append(sb.toString() + "\n");
+            dfs(pre[depth ++], 0, num);
+            sb.append("\n");
         }
-        System.out.println(answer.toString());
+        System.out.println(sb.toString());
     }
 
-    public void dfs(int root, int left, int right, int[] pre, int[] in, int[] idx){
-        if(pos < in.length){
-            int next = pre[pos];
-            if(left < idx[next] && idx[next] < idx[root]){
-                pos ++;
-                dfs(next, left, idx[root], pre, in, idx);
+    public void dfs(int root, int l, int r){
+        if(depth < pre.length){
+            int next_num = pre[depth];
+            int next_idx = index[next_num];
+
+            if(l <= next_idx && next_idx < index[root]){
+                depth ++;
+                dfs(next_num, l, index[root]);
             }
         }
-        if(pos < in.length){
-            int next = pre[pos];
-            if(right > idx[next] && idx[root] < idx[next]){
-                pos ++;
-                dfs(next, idx[root], right, pre, in, idx);
+
+        if(depth < pre.length){
+            int next_num = pre[depth];
+            int next_idx = index[next_num];
+
+            if(index[root] < next_idx && next_idx < r){
+                depth ++;
+                dfs(next_num, index[root], r);
             }
         }
         sb.append(root + " ");
