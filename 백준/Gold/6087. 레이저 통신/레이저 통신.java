6590d0c887ffd8544dbfea0bb4b430df09eb3ae7
BHM <bana_na21@naver.com>
2024-01-16 15:45:36 +0900

    [Gold III] Title: 레이저 통신, Time: 180 ms, Memory: 16336 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.java
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.java
@@ -1,74 +1,93 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    static int N, M;
-    static char[][] map;
-    static int[] start, end;
-    static int[][][] check;
-    static int[][] dir = {{ -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }};
-
+class Main {
     public static void main(String[] args) throws IOException {
+        Solution sol = new Solution();
+        sol.solution();
+    }
+}
+
+class Solution {
+
+    int W, H;
+    char[][] map;
+    int[] start = new int[2], end = new int[2];
+    int[][] dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+
+    public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        map = new char[N][M];
-        check = new int[4][N][M];
 
-        for(int i = 0; i < N; i ++){
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        map = new char[H][W];
+
+        boolean flag = false;
+        for(int r = 0; r < H; r ++){
             String str = br.readLine();
-            for(int j = 0; j < M; j ++){
-                map[i][j] = str.charAt(j);
-                if(str.charAt(j) == 'C'){
-                    if(start == null) start = new int[] { i, j };
-                    else end = new int[] { i, j };
+            for(int c = 0; c < W; c ++){
+                map[r][c] = str.charAt(c);
+                if(!flag && map[r][c] == 'C'){
+                    flag = true;
+                    start = new int[] { r, c };
+                }else if(flag && map[r][c] == 'C'){
+                    end = new int[] { r, c };
                 }
             }
         }
-        
+
         System.out.println(bfs());
     }
 
-    public static int bfs(){
-        int min = 987654321;
-        Queue<int[]> que = new LinkedList<>();
-        for(int i = 0; i < 4; i ++){
-            for(int j = 0; j < N; j ++){
-                Arrays.fill(check[i][j], 987654321);
+    public int bfs(){
+        int ret = 987654321;
+        int[][][] v = new int[4][H][W];
+        for(int j = 0; j < 4; j ++){
+            for(int i = 0; i < H; i ++){
+                Arrays.fill(v[j][i], 987654321);
             }
         }
-        
-        for(int i = 0; i < 4; i ++){
-            que.offer(new int[] { start[0], start[1], i, 0 }); // r, c, dir, time;
-            check[i][start[0]][start[1]] = 0;
-        }
+        // [r, c, cnt, dir, flag]
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                return o1[2] - o2[2];
+            }
+        });
 
-        while(!que.isEmpty()){
-            int[] cur = que.poll();
+        pq.offer(new int[] { start[0], start[1], 0, -1, 1 });
+        v[0][start[0]][start[1]] = 0;
+        v[1][start[0]][start[1]] = 0;
+        v[2][start[0]][start[1]] = 0;
+        v[3][start[0]][start[1]] = 0;
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
             if(cur[0] == end[0] && cur[1] == end[1]){
-                min = Math.min(min, cur[3]);
+                ret = Math.min(ret, cur[2]);
                 continue;
             }
             for(int d = 0; d < 4; d ++){
-                int installed = 0;
-                int nr = cur[0] + dir[d][0];
-                int nc = cur[1] + dir[d][1];
-                if(isOutRange(nr, nc) || map[nr][nc] == '*' || check[d][nr][nc] <= cur[3]) continue;
-                if(d == (cur[2] + 3) % 4 || d == (cur[2] + 1) % 4)
-                    installed ++;
-                else if(d == (cur[2] + 2) % 4) continue;
-                check[d][nr][nc] = cur[3] + installed;
-                que.offer(new int[] { nr, nc, d, cur[3] + installed });
+                int cnt = cur[2];
+                int nr = cur[0] + dirs[d][0];
+                int nc = cur[1] + dirs[d][1];
+                if(isOutRange(nr, nc) || v[d][nr][nc] <= cur[2] || map[nr][nc] == '*') continue;
+                // 출발지점이 아니고 현재 방향에서 변경해야할 경우 count
+                if(cur[4] == 0 && cur[3] != d){
+                    cnt ++;
+                }
+
+                v[d][nr][nc] = cnt;
+                pq.offer(new int[] { nr, nc, cnt, d, 0 });
             }
         }
 
-        return min;
+        return ret;
     }
 
-    public static boolean isOutRange(int r, int c){
-        return (r < 0 || c < 0 || r >= N || c >= M);
+    public boolean isOutRange(int r, int c){
+        return r < 0 || c < 0 || r >= H || c >= W;
     }
 }
+
