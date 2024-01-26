55ff2c11d4754d6f5ecefa6ee9cc4d61d1601b62
BHM <bana_na21@naver.com>
2024-01-26 20:40:16 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 252 ms, Memory: 18832 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
@@ -11,7 +11,12 @@ class Main {
 class Solution {
 
     int answer = 0;
+    int[] pos_arr = new int[4];
+    int[] level = new int[4];
     int[] dice = new int[10];
+    boolean[][] v = new boolean[4][30];
+    boolean[] isEnd = new boolean[4];
+
     int[][] map = {
         {0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40},
         {0,2,4,6,8,10,13,16,19,25,30,35,40},
@@ -27,63 +32,53 @@ class Solution {
             dice[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(0, new int[10]);
-        System.out.println(answer);
+        System.out.println(dfs(0, 0));
     }
 
-    public void dfs(int depth, int[] arr){
+    public int dfs(int depth, int score){
         if(depth == 10){
-            int cnt = simulation(arr);
-            answer = Math.max(answer, cnt);
-            return;
+            return score;
         }
 
-        for(int i = 0; i < 4; i ++){
-            arr[depth] = i;
-            dfs(depth + 1, arr);
-        }
-    }
-
-    public int simulation(int[] arr){
         int ret = 0;
-        int[] pos = new int[4];
-        int[] level = new int[4];
-        boolean[][] v = new boolean[4][30];
-        boolean[] isEnd = new boolean[4];
-        for(int i = 0; i < arr.length; i ++){
-            int idx = arr[i]; // 현재 선택한 말의 idx
-            int nl = level[idx];
-            int move_cnt = dice[i];
-            int next = pos[idx] + move_cnt;
+        for(int i = 0; i < 4; i ++){
+            int nl = level[i];
+            int move_cnt = dice[depth];
+            int pl = level[i];
+            int pp = pos_arr[i];
+            
+            if(pos_arr[i] >= map[nl].length){
+                continue;
+            }
 
-            if(isEnd[idx]) continue;
-            if(level[idx] == 0){
-                if(next == 5){
+            pos_arr[i] += move_cnt;
+            if(level[i] == 0){
+                if(pos_arr[i] == 5){
                     nl = 1;
-                }else if(next == 10){
+                }else if(pos_arr[i] == 10){
                     nl = 2;
-                }else if(next == 15){
+                }else if(pos_arr[i] == 15){
                     nl = 3;
                 }
             }
-            if(!isImpossible(idx, nl, next, v, level, pos)){
-                v[level[idx]][pos[idx]] = false;
-                if(next < map[nl].length){
-                    v[nl][next] = true;
-                    level[idx] = nl;
-                    pos[idx] = next;
-                    ret += map[nl][next];
-                }else{
-                    isEnd[idx] = true;
-                }
-            }else{
-                return -1;
+            if(!isImpossible(i, nl, pos_arr[i])){
+                level[i] = nl;
+                v[pl][pp] = false;
+                v[nl][pos_arr[i]] = true;
+                
+                int num = pos_arr[i] < map[nl].length ? map[nl][pos_arr[i]] : 0;
+                ret = Math.max(ret, dfs(depth + 1, score + num));
+                
+                v[nl][pos_arr[i]] = false;
+                v[pl][pp] = true;
+                level[i] = pl;
             }
+            pos_arr[i] = pp;
         }
         return ret;
     }
 
-    public boolean isImpossible(int idx, int lv, int pos, boolean[][] v, int[] level, int[] pos_arr){
+    public boolean isImpossible(int idx, int lv, int pos){
         if(pos >= map[lv].length) return false;
 
         if((lv == 1 && pos == 9) || (lv == 2 && pos == 13) || (lv == 3 && pos == 19)){
@@ -99,7 +94,7 @@ class Solution {
             // 40
             return v[0][20] || v[1][12] || v[2][16] || v[3][22];
         }
-        
+
         for(int i = 0; i < 4; i ++){
             if(i == idx) continue;
             if(level[i] == lv && pos_arr[i] == pos){
