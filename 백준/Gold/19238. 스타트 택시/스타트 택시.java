3bd2c8f3a3c2967a10b5c7129ee8447b430fe68b
BHM <bana_na21@naver.com>
2024-02-14 13:07:21 +0900

    [Gold II] Title: 스타트 택시, Time: 232 ms, Memory: 26056 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -1,9 +1,7 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         Solution sol = new Solution();
         sol.solution();
     }
@@ -11,22 +9,21 @@ public class Main {
 
 class Solution {
 
-    public int N, M, K, tr, tc;
-    public int[][] map, dirs = {
-        { -1, 0 },
-        { 0, -1 },
-        { 0, 1 },
-        { 1, 0}
-    };
+    int N, M, K;
+    int[] taxi_pos = new int[2];
+    int[][] map, peoples;
+    int[][] dirs = {{ -1, 0 },{ 0, -1 },{ 0, 1 },{ 1, 0 }};
 
-    public void solution() throws IOException {
+    public void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+
         map = new int[N][N];
+        peoples = new int[M][2];
 
         for(int i = 0; i < N; i ++){
             st = new StringTokenizer(br.readLine(), " ");
@@ -36,110 +33,107 @@ class Solution {
         }
 
         st = new StringTokenizer(br.readLine(), " ");
+        int r = Integer.parseInt(st.nextToken()) - 1;
+        int c = Integer.parseInt(st.nextToken()) - 1;
+        taxi_pos = new int[] { r, c };
 
-        tr = Integer.parseInt(st.nextToken()) - 1;
-        tc = Integer.parseInt(st.nextToken()) - 1;
-
-        int[][] path = new int[M][4];
         for(int i = 0; i < M; i ++){
             st = new StringTokenizer(br.readLine(), " ");
+            int sr = Integer.parseInt(st.nextToken()) - 1;
+            int sc = Integer.parseInt(st.nextToken()) - 1;
 
-            int pr = Integer.parseInt(st.nextToken()) - 1;
-            int pc = Integer.parseInt(st.nextToken()) - 1;
-            int gr = Integer.parseInt(st.nextToken()) - 1;
-            int gc = Integer.parseInt(st.nextToken()) - 1;
-
-            map[pr][pc] = 2 + i;
-            path[i] = new int[] { pr, pc, gr, gc };
+            map[sr][sc] = i + 2;
+            int dr = Integer.parseInt(st.nextToken()) - 1;
+            int dc = Integer.parseInt(st.nextToken()) - 1;
+            peoples[i] = new int[] { dr, dc };
         }
 
         for(int i = 0; i < M; i ++){
-            // people idx, remain fuel
-            int[] info = findPeople();
-            info[0] -= 2;
-            if(info[0] <= -1){
-                System.out.println(-1);
-                return;
+            if(!find()){
+                K = -1;
+                break;
             }
-
-            K -= info[1];
-            int gain_fuel = destination(path[info[0]]);
-            if(gain_fuel <= -1){
-                System.out.println(-1);
-                return;
-            }
-            K -= gain_fuel;
-            K += 2 * gain_fuel;
         }
 
         System.out.println(K);
     }
 
-    public int destination(int[] tmp){
+    public boolean find(){
         boolean[][] v = new boolean[N][N];
-        Queue<int[]> q = new LinkedList<>(); // [r, c, 연료 소모량]
-
-        v[tmp[0]][tmp[1]] = true;
-        q.offer(new int[] { tr, tc, 0 });   
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            if(cur[0] == tmp[2] && cur[1] == tmp[3]){
-                tr = cur[0];
-                tc = cur[1];
-                return cur[2]; // 소비한 연료
-            }
-            if(cur[2] == K) continue;
-            for(int d = 0; d < 4; d ++){
-                int nr = cur[0] + dirs[d][0];
-                int nc = cur[1] + dirs[d][1];
-                if(nr < 0 || nc < 0 || nr >= N || nc >= N || v[nr][nc] || map[nr][nc] == 1) continue;
-                v[nr][nc] = true;
-                q.offer(new int[] { nr, nc, cur[2] + 1 });
-            }
-        }
-
-        return -1;
-    }
-
-    public int[] findPeople(){
-        boolean[][] v = new boolean[N][N];
-        Queue<int[]> q = new LinkedList<>(); // [r, c, 연료 소모량]
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<>(){
+        Queue<int[]> q = new LinkedList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
             @Override
             public int compare(int[] o1, int[] o2){
-                if(o1[3] != o2[3]) return o1[3] - o2[3];
+                if(o1[2] != o2[2]) return o1[2] - o2[2];
                 else if(o1[0] != o2[0]) return o1[0] - o2[0];
                 return o1[1] - o2[1];
             }
         });
-        v[tr][tc] = true;
-        q.offer(new int[] { tr, tc, 0 });   
+
+        q.offer(new int[] { taxi_pos[0], taxi_pos[1], 0 });
+        v[taxi_pos[0]][taxi_pos[1]] = true;
 
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            if(map[cur[0]][cur[1]] >= 2){
-                int idx = map[cur[0]][cur[1]];
-                pq.offer(new int[] { cur[0], cur[1], idx, cur[2] });
+            if(map[cur[0]][cur[1]] > 1){
+                pq.offer(new int[] { cur[0], cur[1], cur[2] });
                 continue;
             }
+
             if(cur[2] == K) continue;
             for(int d = 0; d < 4; d ++){
                 int nr = cur[0] + dirs[d][0];
                 int nc = cur[1] + dirs[d][1];
-                if(nr < 0 || nc < 0 || nr >= N || nc >= N || v[nr][nc] || map[nr][nc] == 1) continue;
+                if(isOutRange(nr, nc) || v[nr][nc] || map[nr][nc] == 1) continue;
                 v[nr][nc] = true;
                 q.offer(new int[] { nr, nc, cur[2] + 1 });
             }
         }
 
-        if(pq.size() == 0)
-            return new int[] { -1, -1 };
-        else{
-            tr = pq.peek()[0];
-            tc = pq.peek()[1];
-            map[pq.peek()[0]][pq.peek()[1]] = 0;
-            return new int[] { pq.peek()[2],  pq.peek()[3] };
+        if(pq.size() == 0) return false;
+        int pr = pq.peek()[0];
+        int pc = pq.peek()[1];
+        K -= pq.peek()[2];
+
+        taxi_pos = new int[] { pr, pc };
+        if(move(map[pr][pc], pr, pc)){
+            map[pr][pc] = 0;
+            return true;
         }
+
+        return false;
+    }
+
+    public boolean move(int idx, int r, int c){
+        boolean[][] v = new boolean[N][N];
+        Queue<int[]> q = new LinkedList<>();
+
+        q.offer(new int[] { r, c, 0, 0 });
+        v[taxi_pos[0]][taxi_pos[1]] = true;
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            if(cur[0] == peoples[idx - 2][0] && cur[1] == peoples[idx - 2][1]){
+                K -= cur[2];
+                K += (cur[2] * 2);
+                taxi_pos = new int[] { cur[0], cur[1] };
+                return true;
+            }
+            if(cur[2] == K) continue;
+            for(int d = 0; d < 4; d ++){
+                int nr = cur[0] + dirs[d][0];
+                int nc = cur[1] + dirs[d][1];
+                if(isOutRange(nr, nc) || v[nr][nc] || map[nr][nc] == 1) continue;
+                v[nr][nc] = true;
+                q.offer(new int[] { nr, nc, cur[2] + 1 });
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isOutRange(int r, int c){
+        return r < 0 || c < 0 || r >= N || c >= N;
     }
 }
+
