41f7bb45693130ba3869392a3908f4444b6f0eb9
BHM <bana_na21@naver.com>
2024-03-04 15:59:19 +0900

    [level 3] Title: 에어컨, Time: 11.13 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/README.md
+++ 프로그래머스/3/214289. 에어컨/README.md
@@ -1,10 +1,10 @@
 # [level 3] 에어컨 - 214289 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214289#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214289#) 
 
 ### 성능 요약
 
-메모리: 88.7 MB, 시간: 12.01 ms
+메모리: 72.9 MB, 시간: 11.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 17:9:33
+2024년 03월 04일 15:59:17
 
 ### 문제 설명
 
--- 프로그래머스/3/214289. 에어컨/에어컨.java
+++ 프로그래머스/3/214289. 에어컨/에어컨.java
@@ -1,48 +1,55 @@
 import java.util.*;
 class Solution {
-    int[][] dp = new int[53][1001];
     public int solution(int temperature, int t1, int t2, int a, int b, int[] onboard) {
-        int answer = 987654321;
-        for(int i = 0; i < 53; i ++){
-            Arrays.fill(dp[i], 987654321);
-        }
-        
         temperature += 11;
         t1 += 11;
         t2 += 11;
         
-        dp[temperature][0] = 0;    
-        for(int i = 0; i < onboard.length - 1; i ++){
+        int[][] dp = new int[53][1001];
+        for(int i = 0; i < 53; i ++){
+            Arrays.fill(dp[i], 987654321);
+        }
+        dp[temperature][0] = 0;
+        
+        
+        // 에어컨이 켜졌을 경우 실내온도는 희망온도와 가까운쪽으로 온도가 바뀜
+        // 실내온도와 희망온도가 같을 때 에어컨을 켜도 실내온도는 바뀌지 않음
+        // 에어컨이 꺼졌을 경우 실내온도는 실외온도와 가까워지는쪽으로 온도가 바뀜
+        for(int i = 0; i < onboard.length; i ++){
             for(int j = 1; j < 52; j ++){
-                if(dp[j][i] == 987654321) continue;
-                if(onboard[i] == 1 && (j < t1 || j > t2)) continue;
-                
-                dp[j + 1][i + 1] = Math.min(dp[j + 1][i + 1], dp[j][i] + a);
-                dp[j - 1][i + 1] = Math.min(dp[j - 1][i + 1], dp[j][i] + a);
-                dp[j][i + 1] = Math.min(dp[j][i + 1], dp[j][i] + b);
-                
-                int next_tmp = j;
-                if(j < temperature){
-                    next_tmp ++;
+                if(dp[j][i] != 987654321){
+                    // i 시간에 탑승객이 있는 경우
+                    if(onboard[i] == 1 && (j < t1 || j > t2)){
+                        continue;
+                    }
+                    
+                    // 에어컨을 킨 경우
+                    dp[j + 1][i + 1] = Math.min(dp[j + 1][i + 1], dp[j][i] + a);
+                    dp[j - 1][i + 1] = Math.min(dp[j - 1][i + 1], dp[j][i] + a);
+                    dp[j][i + 1] = Math.min(dp[j][i + 1], dp[j][i] + b);
+                    
+                    int next_tmp = j;
+                    if(temperature > j){
+                        next_tmp ++;
+                    }else if(temperature < j){
+                        next_tmp --;
+                    }
+                    
+                    dp[next_tmp][i + 1] = Math.min(dp[next_tmp][i + 1], dp[j][i]);    
                 }
-
-                if(j > temperature){
-                    next_tmp --;
-                }
-
-                dp[next_tmp][i + 1] = Math.min(dp[next_tmp][i + 1], dp[j][i]);
             }
         }
         
+        int min = 987654321;
         for(int i = onboard.length - 1; i >= 0; i --){
             if(onboard[i] == 1){
-                for(int j = 0; j < 52; j ++){
-                    if(j >= t1 && j <= t2)
-                        answer = Math.min(answer, dp[j][i]);
-                }
+                for(int j = t1; j <= t2; j ++){
+                    min = Math.min(dp[j][i], min);
+                }   
                 break;
             }
         }
-        return answer;
+        
+        return min;
     }
 }
