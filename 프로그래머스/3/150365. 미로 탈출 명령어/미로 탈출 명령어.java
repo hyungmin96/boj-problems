689a519148b4771b5fbafa4027337a8eeb74886d
BHM <bana_na21@naver.com>
2024-03-05 11:12:01 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.05 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,47 +1,48 @@
 import java.util.*;
 class Solution {
-    
-    class Pair{
-        int r, c, cnt;
-        String path;
-        public Pair(int r, int c, int cnt, String path){
-            this.r = r;
-            this.c = c;
-            this.cnt = cnt;
-            this.path = path;
-        }
-    }
-    boolean flag = false;
-    String answer = "impossible";
-    int[][] v;
-    char[] ch = new char[] { 'd', 'l', 'r', 'u' };
-    int[][] dirs = {{ 1, 0 }, { 0, -1 }, { 0, 1 }, { -1, 0 }};
-    
+    // 현재위치에서 (r, c)까지 남은 거리 k, 맨해튼 거리
+    // 남은 거리 k가 홀수이면 짝수번째 방문지점에 방문할 수 없음
+    // 남은 거리 k가 짝수이면 홀수번째 방문지점 방문할 수 없음
+    int N, M, R, C;
+    char[] chs = new char[] { 'd', 'l', 'r', 'u' };
+    int[][] dirs = {{1,0},{0,-1},{0,1},{-1,0}};
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        v = new int[n + 1][m + 1];
-        dfs(n, m, x, y, r, c, k, "");
-        return answer;
+        N = n;
+        M = m;
+        R = r - 1;
+        C = c - 1;
+        
+        return dfs(x - 1, y - 1, k, new StringBuilder());
     }
     
-    public void dfs(int n, int m, int x, int y, int r, int c, int k, String cur){
-        if(flag || k < 0) return;
-        if(k == 0 && x == r && y == c){
-            answer = cur;
-            flag = true;
-            return;
+    public String dfs(int r, int c, int k, StringBuilder sb){
+        if(r == R && c == C && k == 0){
+            return sb.toString();
         }
+        
+        String ret = "";
         for(int d = 0; d < 4; d ++){
-            int nr = x + dirs[d][0];
-            int nc = y + dirs[d][1];
-            if(isOutOfRange(n, m, nr, nc)) continue;
-            int dist = Math.abs(r - nr) + Math.abs(c - nc);
-            if(k - 1 >= dist && (dist % 2) - (k - 1) % 2 == 0){
-                dfs(n, m, nr, nc, r, c, k - 1, cur + ch[d]);
+            int nr = r + dirs[d][0];
+            int nc = c + dirs[d][1];
+            if(isOutRange(nr, nc)){
+                continue;
+            }
+            int dist = Math.abs(nr - R) + Math.abs(nc - C);
+            if(dist > k){
+                continue;
+            }
+            if((dist % 2 == 0 && (k - 1) % 2 == 0) || (dist % 2 != 0 && (k - 1) % 2 != 0)){
+                sb.append(chs[d]);
+                if(ret.length() == 0){
+                    ret = dfs(nr, nc, k - 1, sb);    
+                }
+                sb.deleteCharAt(sb.length() - 1);
             }
         }
+        return ret.equals("") ? "impossible" : ret;
     }
     
-    public boolean isOutOfRange(int n, int m, int r, int c){
-        return r < 1 || c < 1 || r > n || c > m;
+    public boolean isOutRange(int r, int c){
+        return r < 0 || c < 0 || r >= N || c >= M;
     }
 }
