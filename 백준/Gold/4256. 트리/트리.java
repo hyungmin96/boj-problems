9f9aa1dd3b642d1aa509e4f84d62725ec369062d
BHM <bana_na21@naver.com>
2024-04-10 22:47:12 +0900

    [Gold II] Title: 트리, Time: 432 ms, Memory: 38188 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -10,58 +10,54 @@ class Main {
 
 class Solution {
 
-    int N, depth = 0;
-    int[] pre, in, index;
+    int depth = 0;
     StringBuilder sb = new StringBuilder();
 
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N; i ++){
+        int tc = Integer.parseInt(br.readLine());
+        while(tc -- > 0){
             depth = 0;
-            int num = Integer.parseInt(br.readLine());
-            
-            pre = new int[num];
-            in = new int[num];
-            index = new int[num + 1];
-
+            int nums = Integer.parseInt(br.readLine());
+            int[] pre = new int[nums];
+            int[] in = new int[nums];
+            int[] index = new int[nums + 1];
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < num; j ++){
-                pre[j] = Integer.parseInt(st.nextToken());
+            for(int i = 0; i < nums; i ++){
+                pre[i] = Integer.parseInt(st.nextToken());
             }
 
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < num; j ++){
-                in[j] = Integer.parseInt(st.nextToken());
-                index[in[j]] = j;
+            for(int i = 0; i < nums; i ++){
+                in[i] = Integer.parseInt(st.nextToken());
+                index[in[i]] = i;
             }
 
-            dfs(pre[depth ++], 0, num);
+            dfs(pre[depth ++], 0, pre.length, pre, in, index);
             sb.append("\n");
         }
         System.out.println(sb.toString());
     }
 
-    public void dfs(int root, int l, int r){
+    public void dfs(int node, int l, int r, int[] pre, int[] in, int[] index){
         if(depth < pre.length){
-            int next_num = pre[depth];
-            int next_idx = index[next_num];
-
-            if(l <= next_idx && next_idx < index[root]){
+            int next = pre[depth];
+            int idx = index[next];
+            if(l <= idx && idx < index[node]){
                 depth ++;
-                dfs(next_num, l, index[root]);
+                dfs(next, l, index[node], pre, in, index);
             }
         }
 
         if(depth < pre.length){
-            int next_num = pre[depth];
-            int next_idx = index[next_num];
-
-            if(index[root] < next_idx && next_idx < r){
+            int next = pre[depth];
+            int idx = index[next];
+            if(index[node] < idx && idx < r){
                 depth ++;
-                dfs(next_num, index[root], r);
+                dfs(next, index[node], r, pre, in, index);
             }
         }
-        sb.append(root + " ");
+
+        sb.append(node + " ");
     }
 }
