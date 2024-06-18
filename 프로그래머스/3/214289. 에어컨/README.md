e8f22237952da82aec608f2dd870b59a7377633e
BHM <bana_na21@naver.com>
2024-06-18 11:33:00 +0900

    [level 3] Title: 에어컨, Time: 10.59 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/README.md
+++ 프로그래머스/3/214289. 에어컨/README.md
@@ -1,10 +1,10 @@
 # [level 3] 에어컨 - 214289 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214289#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214289) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 11.13 ms
+메모리: 80 MB, 시간: 10.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 15:59:17
+2024년 06월 18일 11:32:57
 
 ### 문제 설명
 
--- 프로그래머스/3/214289. 에어컨/에어컨.java
+++ 프로그래머스/3/214289. 에어컨/에어컨.java
@@ -1,55 +1,57 @@
 import java.util.*;
 class Solution {
+    int[][] dp = new int[51][1001]; // 온도|시간
     public int solution(int temperature, int t1, int t2, int a, int b, int[] onboard) {
-        temperature += 11;
-        t1 += 11;
-        t2 += 11;
+        int answer = 987654321;
         
-        int[][] dp = new int[53][1001];
-        for(int i = 0; i < 53; i ++){
+        temperature += 10;
+        t1 += 10;
+        t2 += 10;
+        for(int i = 0; i < 51; i ++){
             Arrays.fill(dp[i], 987654321);
         }
+        
         dp[temperature][0] = 0;
-        
-        
-        // 에어컨이 켜졌을 경우 실내온도는 희망온도와 가까운쪽으로 온도가 바뀜
-        // 실내온도와 희망온도가 같을 때 에어컨을 켜도 실내온도는 바뀌지 않음
-        // 에어컨이 꺼졌을 경우 실내온도는 실외온도와 가까워지는쪽으로 온도가 바뀜
-        for(int i = 0; i < onboard.length; i ++){
-            for(int j = 1; j < 52; j ++){
-                if(dp[j][i] != 987654321){
-                    // i 시간에 탑승객이 있는 경우
-                    if(onboard[i] == 1 && (j < t1 || j > t2)){
-                        continue;
-                    }
-                    
-                    // 에어컨을 킨 경우
-                    dp[j + 1][i + 1] = Math.min(dp[j + 1][i + 1], dp[j][i] + a);
-                    dp[j - 1][i + 1] = Math.min(dp[j - 1][i + 1], dp[j][i] + a);
-                    dp[j][i + 1] = Math.min(dp[j][i + 1], dp[j][i] + b);
-                    
-                    int next_tmp = j;
-                    if(temperature > j){
-                        next_tmp ++;
-                    }else if(temperature < j){
-                        next_tmp --;
-                    }
-                    
-                    dp[next_tmp][i + 1] = Math.min(dp[next_tmp][i + 1], dp[j][i]);    
+        for(int i = 0; i < onboard.length - 1; i ++){
+            for(int j = 0; j < 51; j ++){
+                if(onboard[i] == 1 && (j < t1 || j > t2)){
+                    continue;
                 }
+                // 온도 1도 낮추기
+                if(j - 1 >= 0){
+                    dp[j - 1][i + 1] = Math.min(dp[j - 1][i + 1], dp[j][i] + a);
+                }
+                // 온도 1도 올리기
+                if(j + 1 < 51){
+                    dp[j + 1][i + 1] = Math.min(dp[j + 1][i + 1], dp[j][i] + a);
+                }
+                // 온도 유지하기
+                dp[j][i + 1] = Math.min(dp[j][i + 1], dp[j][i] + b);
+                
+                // 에어컨 종료한 경우
+                int next = j;
+                if(next > temperature){
+                    next --;
+                }else if(next < temperature){
+                    next ++;
+                }
+                dp[next][i + 1] = Math.min(dp[next][i + 1], dp[j][i]);
             }
         }
         
-        int min = 987654321;
-        for(int i = onboard.length - 1; i >= 0; i --){
-            if(onboard[i] == 1){
-                for(int j = t1; j <= t2; j ++){
-                    min = Math.min(dp[j][i], min);
-                }   
+        for(int j = onboard.length - 1; j >= 0; j --){
+            if(onboard[j] == 1){
+                for(int i = t1; i <= t2; i ++){
+                    answer = Math.min(answer, dp[i][j]);
+                }      
                 break;
             }
         }
         
-        return min;
+        return answer;
     }
 }
+
+// 실내온도와 실외온도는 (0분) 일때 동일함
+// 에어컨을 켜서 희망온도에 가까워지게 1도 상승 하강 유지 가능
+// 에어컨 종료 시 실내온도가 실외온도와 가까워지는 쪽으로 1도 상승 또는 하강
