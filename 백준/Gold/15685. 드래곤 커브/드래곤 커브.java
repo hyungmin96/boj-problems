fbb7fd210c7f1ff0e4091a665141aa7f260cce03
BHM <bana_na21@naver.com>
2025-05-21 23:24:29 +0900

    [Gold III] Title: 드래곤 커브, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -1,53 +1,56 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
-
 public class Main {
 
-    static class Pair{
-        int x, y, d, g;
-        public Pair(int x, int y, int d, int g){ this.x = x; this.y = y; this.d = d; this.g = g; }
-    }
+    static boolean[][] v = new boolean[101][101];
+    static int[][] dirs = {{0,1},{-1,0},{0,-1},{1,0}};
 
-    static int[][] dirs = {
-        { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 0 }
-    };
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        boolean[][] map = new boolean[150][150];
-        for(int i = 0; i < n; i ++){
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
             int g = Integer.parseInt(st.nextToken());
 
-            map[y][x] = true;
-            ArrayList<Integer> list = new ArrayList<>();
-            list.add(d);
-            y += dirs[d][0];
-            x += dirs[d][1];
-            map[y][x] = true;
-            for(int j = 0; j < g; j ++){
-                int size = list.size() - 1;
+            v[y][x] = true;
+            ArrayList<Integer> cur = new ArrayList<>();
+            
+            int nr = y + dirs[d][0];
+            int nc = x + dirs[d][1];
+            v[nr][nc] = true;
+
+            cur.add(d);
+            for(int j = 1; j <= g; j ++){
+                int size = cur.size() - 1;
                 for(int k = size; k >= 0; k --){
-                    int cd = (list.get(k) + 1) % 4;
-                    y += dirs[cd][0];
-                    x += dirs[cd][1];
-                    map[y][x] = true;
-                    list.add(cd);
+                    int nd = (cur.get(k) + 1) % 4;
+
+                    nr += dirs[nd][0];
+                    nc += dirs[nd][1];
+
+                    v[nr][nc] = true;
+                    cur.add(nd);
                 }
             }
         }
-        
-        int answer = 0;
-        for(int r = 0; r < 149; r ++){
-            for(int c = 0; c < 149; c ++){
-                if(map[r][c] && map[r + 1][c] && map[r][c + 1] && map[r + 1][c + 1]){
-                    answer ++;
+
+        System.out.println(getRectangleNums());
+    }
+
+    public static int getRectangleNums(){
+        int ret = 0;
+        for(int i = 0; i < 100; i ++){
+            for(int j = 0; j < 100; j ++){
+                if(v[i][j] && v[i + 1][j] && v[i][j + 1] && v[i + 1][j + 1]){
+                    ret ++;
                 }
             }
         }
-        System.out.println(answer);
+        return ret;
     }
 }
