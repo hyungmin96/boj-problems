75016e514788e230d7f12674d3ae7135ce868929
BHM <bana_na21@naver.com>
2026-01-02 16:45:35 +0900

    [Gold V] Title: 벼락치기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,26 +1,12 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(100001)
 
 N, T = map(int, input().split())
-study_list = []
-for _ in range(N):
-    study_list.append(list(map(int, input().split())))
+study_list = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[-1] * 10001 for _ in range(N+1)]
-def dfs(depth, time):
-    if depth == N:
-        return 0
-    
-    if dp[depth][time] != -1:
-        return dp[depth][time]
-    
-    t, s, res = study_list[depth][0], study_list[depth][1], 0
-    if time >= t:
-        res = max(res, dfs(depth+1, time - t) + s)
+dp = [0] * (T + 1)
+for time_cost, score in study_list:
+    for current_time in range(T, time_cost - 1, -1):
+        dp[current_time] = max(dp[current_time], dp[current_time - time_cost] + score)
 
-    res = max(res, dfs(depth + 1, time))
-    dp[depth][time] = max(dp[depth][time], res)
-    return dp[depth][time]
-
-print(dfs(0,T))
+print(dp[T])
