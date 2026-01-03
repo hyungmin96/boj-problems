bb5623368259df909a730621bcb9e972ba3860cc
BHM <bana_na21@naver.com>
2026-01-03 19:25:25 +0900

    [Silver IV] Title: Maximum Subarray, Time: 92 ms, Memory: 237476 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.py
@@ -3,24 +3,14 @@ sys.setrecursionlimit(100001)
 
 input = sys.stdin.readline
 
-ans = []
 T = int(input())
 for _ in range(T):
     N = int(input())
+    dp = [0] * N
     arr = list(map(int, input().split()))
+    dp[0] = arr[0]
 
-    def dfs(idx):
-        if idx == len(arr):
-            return -float('inf')
-        
-        cur, res = 0, -float('inf')
-        for i in range(idx, len(arr)):
-            cur += arr[i]
-            res = max(res, cur)
+    for i in range(1, N):
+        dp[i] = max(dp[i-1] + arr[i], arr[i])
 
-        res = max(res, dfs(idx + 1))
-        return res
-
-    ans.append(dfs(0))
-
-print("\n".join(map(str, ans)))
+    print(max(dp))
