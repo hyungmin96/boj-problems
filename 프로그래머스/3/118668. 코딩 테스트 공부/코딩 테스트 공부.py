57db467bfc6b80dd3a7e148ee3a2c77a17d5a7f1
BHM <bana_na21@naver.com>
2026-01-30 20:16:36 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 413.94 ms, Memory: 9.62 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,40 +1,33 @@
-# 알고/코딩력이 주어짐.
-# 주어진 모든 문제를 풀 수 있는 알고/코딩력을 얻게되는데까지 걸리는 최소시간.
 import sys
 sys.setrecursionlimit(1000000)
 def solution(alp, cop, problems):
-
-    max_alp, max_cop = -1, -1
-    dp = [[100001] * 181 for _ in range(181)]
     
-    for alp_req, cop_req, _, _, _ in problems:
-        max_alp = max(alp_req, max_alp)
-        max_cop = max(cop_req, max_cop)
-    
-    alp = min(alp, max_alp)
-    cop = min(cop, max_cop)
-    
-    def dfs(alp_cur, cop_cur):
-        alp_cur = min(alp_cur, max_alp)
-        cop_cur = min(cop_cur, max_cop)
+    ta, tc = 0, 0 # 목표 알고력, 코딩력
+    for i in range(len(problems)):
+        alp_req, cop_req, alp_rwd, cop_rwd, cost = problems[i]
+        ta = max(ta, alp_req)
+        tc = max(tc, cop_req)
         
-        if alp_cur >= max_alp and cop_cur >= max_cop:
+    alp, cop = min(alp, ta), min(cop, tc)
+    dp = [[float('inf')] * 181 for _ in range(181)]
+
+    def dfs(alp, cop):
+        alp, cop = min(alp, ta), min(cop, tc)
+        if alp >= ta and cop >= tc:
             return 0
+        if dp[alp][cop] != float('inf'):
+            return dp[alp][cop]
 
-        if dp[alp_cur][cop_cur] != 100001:
-            return dp[alp_cur][cop_cur]
+        # 1 기다리고 alp/cop 증가
+        res = 1000000000
+        dp[alp][cop] = res
+        res = min(dfs(alp + 1, cop) + 1, dfs(alp, cop + 1) + 1)
+        for i in range(len(problems)):
+            alp_req, cop_req, alp_rwd, cop_rwd, cost = problems[i]
+            if alp >= alp_req and cop >= cop_req:
+                res = min(res, dfs(alp + alp_rwd, cop + cop_rwd) + cost)
         
-        dp[alp_cur][cop_cur] = 100002
-        res = 100002
-        # 코딩력 + 1
-        res = min(res, dfs(alp_cur, cop_cur+1) + 1)
-        # 알고력 + 1
-        res = min(res, dfs(alp_cur+1, cop_cur) + 1)
-        for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
-            if alp_cur >= alp_req and cop_cur >= cop_req:
-                res = min(res, dfs(alp_cur + alp_rwd, cop_cur + cop_rwd) + cost)
-        
-        dp[alp_cur][cop_cur] = min(dp[alp_cur][cop_cur], res)
-        return dp[alp_cur][cop_cur]
+        dp[alp][cop] = res
+        return res
     
     return dfs(alp, cop)
