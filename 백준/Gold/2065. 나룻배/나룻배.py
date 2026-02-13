296623d4b42433a1a4ed66d822b23cc604f89153
BHM <bana_na21@naver.com>
2026-02-13 12:02:31 +0900

    [Gold II] Title: 나룻배, Time: 156 ms, Memory: 115060 KB -BaekjoonHub

--- 백준/Gold/2065. 나룻배/나룻배.py
+++ 백준/Gold/2065. 나룻배/나룻배.py
@@ -1,9 +1,10 @@
-import sys, heapq
+import sys
+from collections import deque
 input = sys.stdin.readline
 
 M, T, N = map(int, input().strip().split())
 
-cur, point, pq = 0, 0, [[],[]]
+queue = [deque(), deque()]
 for i in range(N):
     time, des = map(str, input().strip().split())
     time = int(time)
@@ -12,59 +13,39 @@ for i in range(N):
     else:
         des = 1
 
-    heapq.heappush(pq[des], [time, des, i])
+    queue[des].append((time, i))
 
-ans, cur_time, cur_des = [], 0, 0
-while pq[0] or pq[1]:
-    cnt, tmp1, tmp2 = 0, [], []
-    # 현재 위치에서 태울 수 있는 경우.
-    if pq[cur_des] and cur_time >= pq[cur_des][0][0]:
-        while(pq[cur_des] and cnt < M and cur_time >= pq[cur_des][0][0]):
+# 입력으로 온 사람들의 도착정보, 도착시간, 현재위치
+ans, cur_time, cur_des = [0] * N, 0, 0
+
+# 왼쪽/오른쪽 선착장에 기다리는 사람이 한 명이라도 존재할 경우 반복
+while queue[0] or queue[1]:
+
+    cnt = 0
+
+    nxt_l = queue[0][0][0] if queue[0] else float('inf')
+    nxt_r = queue[1][0][0] if queue[1] else float('inf')
+
+    # 각 선착장에서 먼저 도착한 사람의 시간.
+    if cur_time < nxt_l and cur_time < nxt_r:
+        cur_time = min(nxt_l, nxt_r)
+    
+    # 현재 위치에 탑승가능한 승객이 있는 경우.
+    if queue[cur_des] and cur_time >= queue[cur_des][0][0]:
+        while(queue[cur_des] and cnt < M and cur_time >= queue[cur_des][0][0]):
             cnt += 1
-            _, _, idx = heapq.heappop(pq[cur_des])
-            tmp2.append(idx)
-        
-    # 왼쪽 선착장의 사람이 먼저 도착하는지 비교.
-    elif not pq[1] or (pq[0] and pq[1] and pq[0][0][0] < pq[1][0][0]):
-        # 현재 시간에 태울 수 있는 사람이 없는 경우.
-        if cur_time < pq[0][0][0]:
-            # 다음 도착하는 사람이 올 때 까지 대기.
-            cur_time += max(0, (pq[0][0][0] - cur_time))
+            _, idx = queue[cur_des].popleft()
+            # 목적지 이동시간 미리 계산
+            ans[idx] = cur_time + T
 
-        if cur_des != 0:
-            # 현재 나룻배가 왼쪽에 없는 경우.
-            cur_des = (cur_des + 1) % 2
-            cur_time += T
+        cur_des = (cur_des+1) % 2
+        cur_time += T
 
-        while(pq[0] and cur_time >= pq[0][0][0] and cnt < M):
-            cnt += 1
-            _, _, idx = heapq.heappop(pq[0])
-            tmp2.append(idx)
+    # 현재 위치에 탑승가능 승객이 없는 경우(반대편 있음)
+    else:
+        # 다음 위치로 이동
+        cur_time += T
+        cur_des = (cur_des+1) % 2
 
-    # 오른쪽 선착장 사람이 먼저 도착하는 경우.
-    elif not pq[0] or (pq[0] and pq[1] and pq[0][0][0] > pq[1][0][0]):
-        # 현재 시간에 태울 수 있는 사람이 없는 경우.
-        if cur_time < pq[1][0][0]:
-            # 다음 도착하는 사람이 올 때 까지 대기.
-            cur_time += max(0, (pq[1][0][0] - cur_time))
-
-        if cur_des != 1:
-            cur_des = (cur_des + 1) % 2
-            cur_time += T
-            
-        while(pq[1] and cur_time >= pq[1][0][0] and cnt < M):
-            cnt += 1
-            _, _, idx = heapq.heappop(pq[1])
-            tmp2.append(idx)
-
-    # 목적지 이동
-    cur_time += T
-    cur_des = (cur_des + 1) % 2
-
-    for i in range(cnt):
-        ans.append((cur_time, tmp2[i]))
-
-ans.sort(key=lambda x: (x[1]))
-
-for i in range(len(ans)):
-    print(ans[i][0])
+for i in ans:
+    print(i)
