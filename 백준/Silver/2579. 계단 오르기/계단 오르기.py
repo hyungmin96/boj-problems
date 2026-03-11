f5b22e29104b6c36a3b2b5e4290122ef6f70847c
BHM <bana_na21@naver.com>
2026-03-11 10:39:06 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 121188 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -2,27 +2,29 @@ import sys
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-N = int(input())
-dp = [[-1] * N for _ in range(3)]  
+N = int(input().strip())
 arr = [int(input().strip()) for _ in range(N)]
-
+dp = [[-1] * len(arr) for _ in range(3)]
 def dfs(idx, cnt):
-    
-    if idx > N-1 or cnt >= 3:
+    if idx >= len(arr):
         return -987654321
     
     if idx == len(arr)-1:
-        return arr[idx]
+        return arr[len(arr)-1]
     
     if dp[cnt][idx] != -1:
         return dp[cnt][idx]
     
-    res = max(dfs(idx+1, cnt+1) + arr[idx], dfs(idx + 2, 1) + arr[idx])
+    res = 0
+    if cnt + 1 < 3:
+        res = max(res, dfs(idx+1,cnt+1) + arr[idx])
+        
+    res = max(res, dfs(idx+2, 1) + arr[idx])
+
     dp[cnt][idx] = res
     return res
 
 if N == 1:
     print(arr[0])
 else:
-    print(max(dfs(0, 1), dfs(1, 1)))
-
+    print(max(dfs(0,1), dfs(1,1)))
