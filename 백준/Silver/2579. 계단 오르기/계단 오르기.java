70084e6098c84787968e364f2574ec4f7135f9ae
BHM <bana_na21@naver.com>
2026-03-11 10:35:02 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -22,21 +22,13 @@ public class Main {
             System.out.println(arr[1] + arr[2]);
         }
         
-        Arrays.fill(dp, -1);
         if(n < 3) return;
         dp[1] = arr[1];
         dp[2] = arr[1] + arr[2];
         dp[3] = Math.max(arr[2], arr[1]) + arr[3];
-
-        dfs(n, arr);
-        // for(int i = 4; i <= n; i ++){
-        //     dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
-        // }
+        for(int i = 4; i <= n; i ++){
+            dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
+        }
         System.out.println(dp[n]);
-    }
-
-    public static int dfs(int n, int[] arr){
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = Math.max(dfs(n - 3, arr) + arr[n - 1], dfs(n - 2, arr)) + arr[n];
     }
 }
